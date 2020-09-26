54502934ddae96ecb4678d91ec3c1242b39f4204
arpithanr <arpithanr98@gmail.com>
2020-09-26 21:05:19 +0530

    Welcome To Dxc

---
+++ SupreethTest/src/main/java/com/dxc/jenkins/DxcTest.java
@@ -0,0 +1,15 @@
+package com.dxc.jenkins;
+
+public class DxcTest {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, Welcome to DXC TECHNOLOGY");
+		
+	
+
+	}
+	
+
+}
